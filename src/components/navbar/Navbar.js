4eb71990c9e37d77b7a59e01df6afb9ab1e1109b
AlexBrown4020 <balex3497@gmail.com>
2023-02-14 20:31:41 +0900

    Feat: Implement Navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import './navbar.css';
+
+export const Navbar = () => {
+    return (
+        <div id='navbar'>
+            <div id='logo-title-container'>
+                <img id='sunset-logo' src='https://iili.io/HEtcis2.jpg' alt='sunset city logo'/>
+                <h1 id='title'>Sunset City</h1>
+            </div>
+            <nav>
+                <button className='nav-button products'>Products</button>
+                <button className='nav-button order'>Order</button>
+                <button className='nav-button news'>News</button>
+                <button className='nav-button contact'>Contact</button>
+            </nav>
+        </div>
+    )
+}
+ 
+export default Navbar;
