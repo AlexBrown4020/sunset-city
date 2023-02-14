b03e3ac433e9fe39a9c72218df2d2462075e5187
AlexBrown4020 <balex3497@gmail.com>
2023-02-14 20:59:42 +0900

    Feat: Implement beginning of banner

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,20 @@
+import './banner.css';
+
+export const Banner = () => {
+    return (
+        <div>
+            <div className='banner-flank left'>
+
+            </div>
+            <div id='banner-container'>
+                <h2>Product Info</h2>
+            </div>
+            <div className='banner-flank left'>
+
+            </div>
+        </div>
+
+    )
+};
+
+export default Banner;
