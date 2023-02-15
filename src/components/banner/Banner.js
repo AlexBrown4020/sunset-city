5e41d9ae4ef71f44c5dfb474ebd88f3f7df5e62b
AlexBrown4020 <balex3497@gmail.com>
2023-02-15 13:39:49 +0900

    Feat: Add banners flanking main banner

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,15 +2,15 @@ import './banner.css';
 
 export const Banner = () => {
     return (
-        <div>
+        <div id='banner'>
             <div className='banner-flank left'>
-
+                <h3>Hi</h3>
             </div>
             <div id='banner-container'>
                 <h2>Product Info</h2>
             </div>
             <div className='banner-flank left'>
-
+                <h3>Hi again</h3>
             </div>
         </div>
 
