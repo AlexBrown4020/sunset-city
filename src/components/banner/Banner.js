18155e2c6d3681707e4d32b57035ba2c9e3cdaac
AlexBrown4020 <balex3497@gmail.com>
2023-02-15 15:05:00 +0900

    Feat: About banner placement and styling

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,18 +2,16 @@ import './banner.css';
 
 export const Banner = () => {
     return (
-        <div id='banner'>
-            <div className='banner-flank left'>
-                <h3>Hi</h3>
-            </div>
-            <div id='banner-container'>
-                <h2>Product Info</h2>
-            </div>
-            <div className='banner-flank left'>
-                <h3>Hi again</h3>
+        <div id='banner-container'>
+            {/* <img alt='a city skyline at sunset' id='banner-image' src='' /> */}
+            <div id='text-container'>
+                <h2 id='banner-title'>About</h2>
+                <div id='about-paragraph-container'>
+                    <p className='about-paragraph'>This is some placeholder text to tell you about the product and entice customers. Have some flavour text, descriptors and exciting accounts about the product</p>
+                    <p className='about-paragraph'>Have some keywords and hooks that grab their attention and imagination. Closing sentence to drive it home.</p>
+                </div>
             </div>
         </div>
-
     )
 };
 
