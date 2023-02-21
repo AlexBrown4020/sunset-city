6c921b1e3dbdf87cefab5e7394d36a57a8130ac3
AlexBrown4020 <balex3497@gmail.com>
2023-02-21 17:33:44 +0900

    Feat: Change padding of product name and change subscription/tier details of placeholder

--- src/components/product-table/ProductTable.js
+++ src/components/product-table/ProductTable.js
@@ -12,12 +12,12 @@ export const ProductTable = () => {
                 </div>
                 <div className='product'>
                     <p className='product-text name'>Product 2</p>
-                    <p className='product-text info'>Product description about what you get, the lowest tier investment</p>
+                    <p className='product-text info'>Product description about what you get, the mid tier investment</p>
                     <p className='product-text-cost cost'>Cost $$</p>
                 </div>
                 <div className='product-bottom'>
                     <p className='product-text name'>Product 3</p>
-                    <p className='product-text info'>Product description about what you get, the lowest tier investment</p>
+                    <p className='product-text info'>Product description about what you get, the high tier investment</p>
                     <p className='product-text-cost cost'>Cost $$$</p>
                 </div>
             </div>
