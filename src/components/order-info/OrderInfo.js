778504a4dfac171cca7f4b2d35b12a3e2fc418cb
AlexBrown4020 <balex3497@gmail.com>
2023-02-21 19:50:13 +0900

    Feat: Show order info placeholder

--- src/components/order-info/OrderInfo.js
+++ src/components/order-info/OrderInfo.js
@@ -7,7 +7,17 @@ export const OrderInfo = () => {
 
             </div>
             <div className='central-order-info'>
-                <h3 id='order-info-title'>Order Info</h3>
+                <div id='sub-container'>
+                    <h3 id='order-info-title'>Order Info</h3>
+                    <p id='order-instructions'>Here are a list of instructions on how to order the products</p>
+                    <div id='order-paragraphs'>
+                        <li className='order-step'>Here is instruction one, do this stuff.  Lorem ipsum blah blah blah blah blah blah blah blah blah</li>
+                        <li className='order-step'>Here is a link to a payment page: <a href='fake'>fake path</a></li>
+                        <li className='order-step'>Email to official vendor, and other vital information: fake@email.com</li>
+                        <li className='order-step'>Other information that might be useful</li>
+                    </div>
+                </div>
+
             </div>
             <div className='flank-container right'>
 
