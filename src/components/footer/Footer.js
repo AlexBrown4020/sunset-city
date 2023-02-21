ed238a04fd93d0d7dc8a280c5b1090b2f20660cf
AlexBrown4020 <balex3497@gmail.com>
2023-02-22 08:58:38 +0900

    Feat: Order layout and more examples of social media

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,8 @@
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { IconButton } from '@mui/material';
-import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 import './footer.css';
 
@@ -10,9 +11,9 @@ export const Footer = () => {
         <div id='footer-container'>
             <div className='footer-content-block'>
                 <h5>Page Information</h5>
-                <p className='footer-p'>Built with React</p>
                 <p className='footer-p'>© All rights reserved</p>
                 <p className='footer-p'>21st February 2023, Alex Brown</p>
+                <p className='footer-p'>Powered by React</p>
             </div>
             <div className='footer-content-block'>
                 <h5>Contact</h5>
@@ -30,7 +31,10 @@ export const Footer = () => {
                         <GitHubIcon sx={{color:'white'}}/>
                     </IconButton>
                     <IconButton href=''>
-                        <LocalGroceryStoreIcon sx={{color:'white'}}/>
+                        <FacebookIcon sx={{color:'white'}}/>
+                    </IconButton>
+                    <IconButton>
+                        <TwitterIcon sx={{color:'white'}}/>
                     </IconButton>
                 </div>
             </div>
