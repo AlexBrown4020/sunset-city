e56707550b25de8af0bd5af35415efa6890e3b75
AlexBrown4020 <balex3497@gmail.com>
2023-02-21 20:25:29 +0900

    Feat: Change background colour of content

--- src/components/product-table/ProductTable.js
+++ src/components/product-table/ProductTable.js
@@ -3,8 +3,8 @@ import './product-table.css';
 export const ProductTable = () => {
     return (
         <div id='product-container'>
-            <h3 id='product-title'>Product List</h3>
             <div id='product-table'>
+                <h3 id='product-title'>Product List</h3>
                 <div className='product'>
                     <p className='product-text name'>Product 1</p>
                     <p className='product-text info'>Product description about what you get, the lowest tier investment</p>
