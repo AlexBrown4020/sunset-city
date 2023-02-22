0b304c6df7db5648e2e28e055b437d9b3d938524
AlexBrown4020 <balex3497@gmail.com>
2023-02-22 13:52:44 +0900

    Feat: Add basic hamburger bar for site navigation

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,42 @@
 import './navbar.css';
+import React, { useState } from 'react';
 
-export const Navbar = () => {
+const Navbar = () => {
+    const [burgerClass, setBurgerClass] = useState('burger-bar unclicked');
+    const [menu, setMenu] = useState('menu hidden');
+    const [isMenuClicked, setIsMenuClicked] = useState(false);
+
+    const updateMenu = () => {
+        if(!isMenuClicked) {
+            setBurgerClass('burger-bar clicked');
+            setMenu('menu visible');
+        } else {
+            setBurgerClass('burger-bar unclicked');
+            setMenu('menu hidden');
+        }
+        setIsMenuClicked(!isMenuClicked);
+    }
+ 
     return (
         <div id='navbar'>
             <div id='logo-title-container'>
                 <img id='sunset-logo' src='https://iili.io/HEtcis2.jpg' alt='sunset city logo'/>
                 <h1 id='title'>Sunset City</h1>
             </div>
-            <nav id='nav-button-container'>
-                <button className='nav-button products'>Products</button>
-                <button className='nav-button order'>Order</button>
-                <button className='nav-button news'>News</button>
-                <button className='nav-button contact'>Contact</button>
+            <nav>
+                <div className='burger-menu' onClick={updateMenu}>
+                    <div className={burgerClass}></div>
+                    <div className={burgerClass}></div>
+                    <div className={burgerClass}></div>
+                </div>
             </nav>
+            <div className={menu}>
+                <ul>Top</ul>
+                <ul>Products</ul>
+                <ul>Order</ul>
+                <ul>News</ul>
+                <ul>Bottom</ul>
+            </div>
         </div>
     )
 }
