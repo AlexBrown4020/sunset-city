5b0cec970d65bb63b3121930c1c316cb8af21e0a
AlexBrown4020 <balex3497@gmail.com>
2023-02-22 15:11:09 +0900

    Feat: Apply custom fonts to all titles

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,7 +3,7 @@ import './banner.css';
 export const Banner = () => {
     return (
         <div id='banner-container'>
-            <h2 id='banner-title'>About</h2>
+            <h2 id='banner-title'>Fantasy-Synthwave RPG System</h2>
             <p className='about-paragraph'>This is some placeholder text to tell you about the product and entice customers. Have some flavour text, descriptors and exciting accounts about the product.
             Have some keywords and hooks that grab their attention and imagination. Closing sentence to drive it home.
             </p>
