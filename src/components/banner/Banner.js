b4d760f26d486118530e92a23869a8db5774bde2
AlexBrown4020 <balex3497@gmail.com>
2023-02-23 10:05:16 +0900

    Feat: Fix display of top content on smaller screens

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,10 +3,12 @@ import './banner.css';
 export const Banner = () => {
     return (
         <div id='banner-container'>
-            <h2 id='banner-title'>Fantasy-Synthwave RPG System</h2>
-            <p className='about-paragraph'>This is some placeholder text to tell you about the product and entice customers. Have some flavour text, descriptors and exciting accounts about the product.
-            Have some keywords and hooks that grab their attention and imagination. Closing sentence to drive it home.
-            </p>
+            <div id='text-container'>
+                <h2 id='banner-title'>Fantasy-Synthwave RPG System</h2>
+                <p className='about-paragraph'>This is some placeholder text to tell you about the product and entice customers. Have some flavour text, descriptors and exciting accounts about the product.
+                Have some keywords and hooks that grab their attention and imagination. Closing sentence to drive it home.
+                </p>
+            </div>
         </div>
     )
 };
