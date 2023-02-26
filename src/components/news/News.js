067223cbaef7852c6adff4717eeb15a14b16ce35
AlexBrown4020 <balex3497@gmail.com>
2023-02-26 15:48:56 +0900

    Feat: Add useFetch hook and deployed news data

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,21 +1,16 @@
 import './news.css';
-
-const mockData = [
-    {title: 'Title 1', content: 'This is some random text content in place of the website owner making announcements and updates to potential clients and investors.', date: '17/02/2023'},
-    {title: 'Title 2', content: 'This is some random text content in place of the website owner making announcements and updates to potential clients and investors.', date: '18/02/2023'},
-    {title: 'Title 3', content: 'This is some random text content in place of the website owner making announcements and updates to potential clients and investors.', date: '19/02/2023'},
-    {title: 'Title 4', content: 'This is some random text content in place of the website owner making announcements and updates to potential clients and investors.', date: '20/02/2023'},
-    {title: 'Title 5', content: 'This is some random text content in place of the website owner making announcements and updates to potential clients and investors.', date: '21/02/2023'},
-    {title: 'Title 6', content: 'This is some random text content in place of the website owner making announcements and updates to potential clients and investors.', date: '22/02/2023'}
-]
+import useFetch from '../../hooks/useFetch';
 
 export const News = () => {
+    const url = `https://sunset-city-api.herokuapp.com/news`;
+    const { data } = useFetch(url);
+
     return (
         <div id='news-container'>
             <h4 id='news-title'>News</h4>
             <div id='content-container'>
                 {
-                    mockData.map((obj) => {
+                    data.map((obj) => {
                         return <div className='news-content'>
                                 <p className='news-content-title'>{obj.title}</p>
                                 <p className='news-content-p'>{obj.content}</p>
