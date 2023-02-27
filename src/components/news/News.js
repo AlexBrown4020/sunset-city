e0fb916bdbac18628e469ec73046f03c7692a00a
AlexBrown4020 <balex3497@gmail.com>
2023-02-27 15:11:49 +0900

    Feat: Implement delete button for news

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -1,20 +1,75 @@
+import { useEffect, useState } from 'react';
+
 import './news.css';
 import useFetch from '../../hooks/useFetch';
 
 export const News = () => {
     const url = `https://sunset-city-api.herokuapp.com/news`;
     const { data } = useFetch(url);
+    const [auth, setAuth] = useState('');
+    const [title, setTitle] = useState('');
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        setAuth(localStorage.getItem('user'));
+        if (auth) {
+            setAuth(auth);
+        }
+    }, [auth]);
+
+    const submitData = async () => {
+        let result = await fetch('https://sunset-city-api.herokuapp.com/news', {
+            method: 'post',
+            body: JSON.stringify({
+                title, message
+            }),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        });
+        result = await result.json();
+        if (result.title) {
+            console.log("Submitted");
+        } else {
+            console.log("Failed");
+        }
+    };
+
+    const deleteData = async (id) => {
+        let news = await fetch(`https://sunset-city-api.herokuapp.com/news/${id}`, {
+          method: 'delete'
+        });
+        news = await news.json();
+        if (news) {
+          alert('News deleted');
+        }
+      };
 
     return (
         <div id='news-container'>
             <h4 id='news-title'>News</h4>
+            {
+                auth ? 
+                <div id='create-container'>
+                    <h3 id='create-news'>Create news...</h3>
+                    <input className='create-input' type='text' placeholder='News Title' onChange={(e) => {
+                            setTitle(e.target.value)}} value={title}></input>
+                    <input className='create-input large-input' type='text' placeholder='News Content' onChange={(e) => {
+                            setMessage(e.target.value)}} value={message}></input>
+                    <button onClick={submitData}>Submit</button>
+                </div>
+                : <></>
+            }
             <div id='content-container'>
                 {
                     data.map((obj) => {
                         return <div className='news-content' key={obj.title}>
-                                <p  className='news-content-title'>{obj.title}</p>
-                                <p  className='news-content-p'>{obj.content}</p>
-                                <p  className='news-content-date'>Posted: {obj.date}</p>
+                                <div id='news-banner'>
+                                    <p  className='news-content-title'>{obj.title}</p>
+                                    <button onClick={() => deleteData(obj._id)}>Delete</button>
+                                </div>
+                                <p  className='news-content-p'>{obj.message}</p>
+                                <p  className='news-content-date'>Posted: {obj.created_date.slice(0,10)}</p>
                             </div>
                     })
                 }
