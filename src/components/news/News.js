5c94b5d5170327af2cdcad634b17e2477f941443
AlexBrown4020 <balex3497@gmail.com>
2023-02-28 16:13:35 +0900

    Feat: Change display of news bar, remove container

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -54,37 +54,35 @@ export const News = () => {
     return (
         <div id='news-container'>
             <h4 id='news-title'>News</h4>
-            <div id='creator-layout'>
                 {
-                    auth ? 
-                    <div id='create-container'>
-                        <h3 id='create-news'>Create news...</h3>
-                        <input className='create-input' type='text' placeholder='News Title' onChange={(e) => {
-                                setTitle(e.target.value)}} value={title}></input>
-                        <input className='create-input large-input' type='text' placeholder='News Content' onChange={(e) => {
-                                setMessage(e.target.value)}} value={message}></input>
-                        <button onClick={submitData}>Submit</button>
-                    </div>
-                    : <></>
-                }
-                <div id='content-container'>
-                    {
-                        data.map((obj) => {
-                            return <div className='news-content' key={obj.title}>
-                                    <div id='news-banner'>
-                                        <p  className='news-content-title'>{obj.title}</p>
-                                        {
-                                            auth ? 
-                                            <button onClick={() => deleteData(obj._id)}>Delete</button>
-                                            : <></>
-                                        }
-                                    </div>
-                                    <p  className='news-content-p'>{obj.message}</p>
-                                    <p  className='news-content-date'>Posted: {obj.created_date.slice(0,10)}</p>
-                                </div>
-                        })
-                    }
+                auth ? 
+                <div id='create-container'>
+                    <h3 id='create-news'>Create news...</h3>
+                    <input className='create-input' type='text' placeholder='News Title' onChange={(e) => {
+                            setTitle(e.target.value)}} value={title}></input>
+                    <textarea className='create-input large-input' type='textarea' placeholder='News Content' onChange={(e) => {
+                            setMessage(e.target.value)}} value={message}></textarea>
+                    <button onClick={submitData}>Submit</button>
                 </div>
+                : <></>
+            }
+            <div id='content-container'>
+                {
+                    data.map((obj) => {
+                        return <div className='news-content' key={obj.title}>
+                                <div id='news-banner'>
+                                    <p  className='news-content-title'>{obj.title}</p>
+                                    {
+                                        auth ? 
+                                        <button onClick={() => deleteData(obj._id)}>Delete</button>
+                                        : <></>
+                                    }
+                                </div>
+                                <p  className='news-content-p'>{obj.message}</p>
+                                <p  className='news-content-date'>Posted: {obj.created_date.slice(0,10)}</p>
+                            </div>
+                    })
+                }
             </div>
         </div>
     )
