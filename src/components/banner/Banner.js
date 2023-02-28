7299ee63ff4e33b68090a77772f7ca8fe06ebcf8
AlexBrown4020 <balex3497@gmail.com>
2023-02-28 11:15:41 +0900

    Feat: Change banner text

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,8 +5,9 @@ export const Banner = () => {
         <div id='banner-container'>
             <div id='text-container'>
                 <h2 id='banner-title'>Fantasy-Synthwave RPG System</h2>
-                <p className='about-paragraph'>This is some placeholder text to tell you about the product and entice customers. Have some flavour text, descriptors and exciting accounts about the product.
-                Have some keywords and hooks that grab their attention and imagination. Closing sentence to drive it home.
+                <p className='about-paragraph'>Imagine a world where magic and monsters were thrust upon a retro Miami; Immortal elves running corporations,
+                orc PI's investigating murders, dwarves fashioning robots out of electronics.  Sunset City is an RPG system inspired by high fantasy and 80s 
+                TV shows like Nightrider and Magnum PI. Dive in to the underbelly and solve that case, detectives!
                 </p>
             </div>
         </div>
